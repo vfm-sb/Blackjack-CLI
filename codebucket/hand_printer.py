8cfa09a14f8e3ab90cbd7fc4514b2057b53863ad
VFM | SB <vfm_sb@proton.me>
2022-12-18 00:27:12 +0200

    Working Version of make_card() -- codebucket/hand_printer.py

--- codebucket/hand_printer.py
+++ codebucket/hand_printer.py
@@ -1,19 +1,31 @@
 """Hand Printer"""
 
-def make_card(card: list[str]):
+def make_card(card: list[str]) -> list:
     card_model = []
     card_model.append("┌───────────┐") # Top
     card_model.append("│           │") # Face: [3] or [2],[3], and [-3] or [-2],[-3]
-    card_model.append("│  @     @  │") # Suit: [3] and [3]
+    card_model.append("│           │") # Suit: [3] and [3]
     card_model.append("│           │") # Empty
-    card_model.append("│     @     │") # Suit: [7]
+    card_model.append("│           │") # Suit: [7]
     card_model.append("│           │") # Empty
-    card_model.append("│  @     @  │") # Suit: [3] and [3]
+    card_model.append("│           │") # Suit: [3] and [3]
     card_model.append("│           │") # Face: [3] or [2],[3], and [-3] or [-2],[-3]
     card_model.append("└───────────┘")
-    # replace '#' in card_model[1] and card_model[-1]
+    # inject card face
     if card[0] != "10":
-        pass
+        card_model[1] = f"│  {card[0]}     {card[0]}  │"
+        card_model[-2] = f"│  {card[0]}     {card[0]}  │"
+    else:
+        card_model[1] = f"│ {card[0]}     {card[0]} │"
+        card_model[-2] = f"│ {card[0]}     {card[0]} │"
+    # inject suit symbol
+    card_model[2] = f"│  {card[1]}     {card[1]}  │"
+    card_model[4] = f"│     {card[1]}     │"
+    card_model[-3] = f"│  {card[1]}     {card[1]}  │"
+    return card_model
+
+def display_card(card: list[str]) -> None:
+    pass
 
 def display_hand(hand: list[str]):
     pass
