f2f8ae884257c24fcb907aed98cb4b77866dfede
VFM | SB <vfm_sb@proton.me>
2022-12-18 00:14:49 +0200

    Removed replacer() and bulk_replacer from hand_printer.py

--- codebucket/hand_printer.py
+++ codebucket/hand_printer.py
@@ -1,22 +1,5 @@
 """Hand Printer"""
 
-def replacer(string, new_string, index, nofail=False):
-    # raise an error if index is outside of the string
-    if not nofail and index not in range(len(string)):
-        raise ValueError("index outside given string")
-    # if not erroring, but the index is still not in the correct range..
-    if index < 0:  # add it to the beginning
-        return new_string + string
-    if index > len(string):  # add it to the end
-        return string + new_string
-    # insert the new string between "slices" of the original
-    return string[:index] + new_string + string[index + 1:]
-
-def bulk_replacer(string: str, new_string: str, indexes: list[int]):
-    for index in indexes:
-        string = replacer(string, new_string, index)
-    return string
-
 def make_card(card: list[str]):
     card_model = []
     card_model.append("┌───────────┐") # Top
