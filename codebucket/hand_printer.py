377fbfb3564fa7a7157858fddf785a4ac7ad0ff9
VFM | SB <vfm_sb@proton.me>
2022-12-18 00:04:02 +0200

    Deadend on the Bulk Replacing -- codebucket/hand_printer.py

--- codebucket/hand_printer.py
+++ codebucket/hand_printer.py
@@ -1,41 +1,46 @@
 """Hand Printer"""
 
-def replacer(string, newstring, index, nofail=False):
+def replacer(string, new_string, index, nofail=False):
     # raise an error if index is outside of the string
     if not nofail and index not in range(len(string)):
         raise ValueError("index outside given string")
     # if not erroring, but the index is still not in the correct range..
     if index < 0:  # add it to the beginning
-        return newstring + string
+        return new_string + string
     if index > len(string):  # add it to the end
-        return string + newstring
+        return string + new_string
     # insert the new string between "slices" of the original
-    return string[:index] + newstring + string[index + 1:]
+    return string[:index] + new_string + string[index + 1:]
+
+def bulk_replacer(string: str, new_string: str, indexes: list[int]):
+    for index in indexes:
+        string = replacer(string, new_string, index)
+    return string
 
 def make_card(card: list[str]):
     card_model = []
     card_model.append("┌───────────┐") # Top
-    card_model.append("│ ##     ## │") # Face: [3] or [2],[3], and [-3] or [-2],[-3]
+    card_model.append("│           │") # Face: [3] or [2],[3], and [-3] or [-2],[-3]
     card_model.append("│  @     @  │") # Suit: [3] and [3]
     card_model.append("│           │") # Empty
     card_model.append("│     @     │") # Suit: [7]
     card_model.append("│           │") # Empty
     card_model.append("│  @     @  │") # Suit: [3] and [3]
-    card_model.append("│ ##     ## │") # Face: [3] or [2],[3], and [-3] or [-2],[-3]
+    card_model.append("│           │") # Face: [3] or [2],[3], and [-3] or [-2],[-3]
     card_model.append("└───────────┘")
     # replace '#' in card_model[1] and card_model[-1]
     if card[0] != "10":
-        card_model[1] = replacer(string=card_model[1], newstring=" ", index=2)
-        card_model[1] = replacer(string=card_model[1], newstring=" ", index=10)
-        card_model[-2] = replacer(string=card_model[-2], newstring=" ", index=2)
-        card_model[-2] = replacer(string=card_model[-2], newstring=" ", index=10)
-        
+        card_model[1] = bulk_replacer(string=card_model[1], new_string=card[0], indexes=[3, 9])
+        card_model[-2] = bulk_replacer(string=card_model[-2], new_string=card[0], indexes=[3, 9])
+    else:
+        card_model[1] = bulk_replacer(string=card_model[1], new_string=card[0], indexes=[2, 9])
+        card_model[-2] = bulk_replacer(string=card_model[-2], new_string=card[0], indexes=[2, 9])
     return card_model
 
 def display_hand(hand: list[str]):
     pass
 
 if __name__ == "__main__":
-    example_card = ["A", "x"]
+    example_card = ["10", "♥"]
     demo = "\n".join(make_card(example_card))
     print(demo)
