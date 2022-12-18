ea4de8914b23c5f7080072f3da8ad09354b7616c
VFM | SB <vfm_sb@proton.me>
2022-12-18 19:13:31 +0200

    Corrections to Type Hints and Added Missing Docstrings

--- blackjack.py
+++ blackjack.py
@@ -4,7 +4,7 @@ __author__ = "VFM | SB"
 __email__ = "vfm_sb@proton.me"
 __copyright__ = "Copyright 2022"
 __license__ = "MIT"
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 __maintainer__ = "VFM | SB"
 __status__ = "Development"
 
@@ -27,7 +27,7 @@ SUITS = {
 
 
 # Functions
-def deck_of_cards(decks: int = 1) -> list[list[str, str]]:
+def deck_of_cards(decks: int = 1) -> list:
     """Generates and Returns Desired Number of Card Decks
     > One Deck Contains 4 Card Suits
     Argument:
@@ -45,7 +45,7 @@ def deck_of_cards(decks: int = 1) -> list[list[str, str]]:
             playing_cards.extend(_playing_cards)
     return playing_cards
 
-def shuffle_cards(playing_cards: list[str], shuffles: int = 1) -> None:
+def shuffle_cards(playing_cards: list, shuffles: int = 1) -> None:
     """Shuffles Given Card Decks in Desired Times
     Arguments:
         playing_cards (list[str]): A List of Game Cards
@@ -56,18 +56,18 @@ def shuffle_cards(playing_cards: list[str], shuffles: int = 1) -> None:
     for _ in range(shuffles):
         shuffle(playing_cards)
 
-def deal_card(playing_cards: list[str]) -> str:
+def deal_card(playing_cards: list) -> list[str, str]:
     """Retrives and then Removes Last Card of the Playing Cards
     Returns:
         playing_cards[-1] (str)
     """
     return playing_cards.pop(-1)
 
-def card_value(card: list[str]) -> int:
+def card_value(card: list[str, str]) -> int:
     """Retrieves and Returns The Card's Value from the CARD_SUIT"""
     return CARD_SUIT[card[0]]
 
-def calculate_hand(hand: list[list]) -> int:
+def calculate_hand(hand: list) -> int:
     """Calculates and Returns Given Hand's Total"""
     sum_of_hand = 0
     aces_count = 0
@@ -85,7 +85,7 @@ def calculate_hand(hand: list[list]) -> int:
         sum_of_hand += aces_count
     return sum_of_hand
 
-def busted(hand: list[str]) -> bool:
+def busted(hand: list) -> bool:
     """Checks If The Hand Exceeds 21 or Not
     Returns:
         True (bool), if hand is over 21
@@ -93,7 +93,8 @@ def busted(hand: list[str]) -> bool:
     """
     return True if calculate_hand(hand) > 21 else False
 
-def make_card(card: list[str]) -> list:
+def make_card(card: list[str, str]) -> list[str]:
+    """Generates CLI Template of a Card"""
     card_model = []
     card_model.append("┌───────────┐") # Top
     card_model.append("│           │") # Face: [3] or [2],[3], and [-3] or [-2],[-3]
@@ -117,16 +118,19 @@ def make_card(card: list[str]) -> list:
     card_model[-3] = f"│  {card[1]}     {card[1]}  │"
     return card_model
 
-def display_card(card: list[str]) -> None:
+def display_card(card: list[str, str]) -> str:
+    """Calls make_card() for Template Card Formation, Joins Pieces,
+    and Returns Formed Card as String"""
     return "\n".join(make_card(card))
 
-def display_hand(hand: list[str], has_facedown_card: bool = False) -> None:
+def display_hand(hand: list, has_facedown_card: bool = False) -> str:
+    """Displays Hand"""
     hand_copy = hand.copy()
     if has_facedown_card:
         hand_copy[0] = ["*", "*"]
     return '\n'.join(map('   '.join, zip(*(make_card(card) for card in hand_copy))))
 
-def blackjack():
+def blackjack() -> None:
     """Blackjack Game Function"""
     playing_cards = deck_of_cards()
     shuffle_cards(playing_cards)
@@ -162,7 +166,7 @@ def blackjack():
         print()
     print()
     # display dealer's full hand
-    print("Dealer's Open Hand is:\n", repr_hand(dealer_hand))
+    print("Dealer's Open Hand is:\n", display_hand(dealer_hand))
     # dealer's play: dealer must hit if hand is less 17
     while calculate_hand(dealer_hand) < 17:
         new_card = deal_card(playing_cards)
@@ -192,6 +196,7 @@ def blackjack():
 
 
 def main():
+    """Main Function"""
     while True:
         system("clear")
         with open("blackjack_logo.txt", "r", encoding="utf-8") as blackjack_logo:
@@ -203,4 +208,3 @@ def main():
 
 
 main()
-
